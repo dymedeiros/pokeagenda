72135b3f342ba7b66217a5df48500989dd72410d
Dyego Medeiros <dyego.medeiros@car10.com.br>
2023-06-12 09:03:57 -0300

    primeiro commit

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  
+  pokemon:PokemonData = {
+    id:0,
+    name:'',
+    sprites:{front_default:''},
+    types:[]
+  }
+  
+  constructor(
+    private service:PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+
+        },
+        error: (err) => console.log(err)
+      }
+    )
+  }
+
+}
